51299b76f63eb1256e862f5e0c575e7c7b5f5f1b
codeiva4u <codeiva@protonmail.com>
2024-02-26 19:31:41 +0530

    update

---
+++ PrmoviesProvider/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+    language = "hi"
+
+    description = "Contains Bharatiya Movies & Webseries from Indian Film Industry and Dubbed Hollywood content, Max Resolution is 1080p"
+
+    authors = listOf("hexated")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=prmovies.green&sz=%size%"
+}
