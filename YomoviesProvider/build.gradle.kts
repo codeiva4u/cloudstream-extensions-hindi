ad2b9b3919ae9e0164c7462e73930d899ad77ab8
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-02-26 22:32:27 +0530

    Update build.gradle.kts

--- YomoviesProvider/build.gradle.kts
+++ YomoviesProvider/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     language = "hi"
     // All of these properties are optional, you can safely remove them
 
-     description = "Includes: Watchomovies(NS*W), Max resolution is 720p in both extensions."
+     description = "Includes: Watchomovies(NS*W), Max resolution is 1080p in both extensions."
      authors = listOf("Hexated")
 
     /**
