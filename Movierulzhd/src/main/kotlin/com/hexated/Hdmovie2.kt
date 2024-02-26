c3ebc4a2066e14a7062effc68b99e6528bb86a89
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-02-27 01:12:53 +0530

    Update Hdmovie2.kt

--- Movierulzhd/src/main/kotlin/com/hexated/Hdmovie2.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Hdmovie2.kt
@@ -15,15 +15,11 @@ class Hdmovie2 : Movierulzhd() {
     override var mainUrl = "https://hdmovie2.tax"
     override var name = "Hdmovie2"
     override val mainPage = mainPageOf(
-        "movies" to "Release Movies",
-        "trending" to "Most Trending",
-        "genre/hindi-dubbed" to "Hindi Dubbed Movies",
-        "genre/bollywood" to "Bollywood Movies",
+        "trending" to "Trending",
+        "movies" to "Movies",
+        "genre/tv-series" to "TV Shows",
         "genre/netflix" to "Netflix",
-        "genre/amazon" to "Amazon Prime",
-        "genre/hotstar" to "Hotstar",
         "genre/zee5-tv-series" to "Zee5",
-        "genre/jio-cinema" to "Jio Cinema",
     )
 
     override suspend fun loadLinks(
@@ -33,12 +29,8 @@ class Hdmovie2 : Movierulzhd() {
         callback: (ExtractorLink) -> Unit
     ): Boolean {
         if (data.startsWith("{")) {
-            val loadData = tryParseJson<LinkData>(data)
-            val source = app.post(
-                url = "$directUrl/wp-admin/admin-ajax.php", data = mapOf(
-                    "action" to "doo_player_ajax", "post" to "${loadData?.post}", "nume" to "${loadData?.nume}", "type" to "${loadData?.type}"
-                ), referer = data, headers = mapOf("Accept" to "*/*", "X-Requested-With" to "XMLHttpRequest"
-                )).parsed<ResponseHash>().embed_url.getIframe()
+            val loadData = tryParseJson<LinkData>(data) ?: return false
+            val source = getSourceUrl(loadData) ?: return false
             if (!source.contains("youtube")) loadExtractor(
                 source,
                 "$directUrl/",
@@ -50,26 +42,55 @@ class Hdmovie2 : Movierulzhd() {
             val id = document.select("meta#dooplay-ajax-counter").attr("data-postid")
             val type = if (data.contains("/movies/")) "movie" else "tv"
 
-            document.select("ul#playeroptionsul > li").map {
-                it.attr("data-nume")
-            }.apmap { nume ->
-                val source = app.post(
-                    url = "$directUrl/wp-admin/admin-ajax.php", data = mapOf(
-                        "action" to "doo_player_ajax", "post" to id, "nume" to nume, "type" to type
-                    ), referer = data, headers = mapOf("Accept" to "*/*", "X-Requested-With" to "XMLHttpRequest")
-                ).parsed<ResponseHash>().embed_url.getIframe()
-                when {
-                    !source.contains("youtube") -> loadExtractor(
-                        source,
-                        "$directUrl/",
-                        subtitleCallback,
-                        callback
-                    )
-                    else -> return@apmap
-                }
+            getNumeList(document)?.forEach { nume ->
+                val source = getSourceUrl(nume, id, type) ?: return@forEach
+                if (!source.contains("youtube")) loadExtractor(
+                    source,
+                    "$directUrl/",
+                    subtitleCallback,
+                    callback
+                )
             }
         }
         return true
+    }
+
+    private fun getSourceUrl(loadData: LinkData): String? {
+        val response = app.post(
+            url = "$directUrl/wp-admin/admin-ajax.php",
+            data = mapOf(
+                "action" to "doo_player_ajax",
+                "post" to "${loadData.post}",
+                "nume" to "${loadData.nume}",
+                "type" to "${loadData.type}"
+            ),
+            referer = data,
+            headers = mapOf("Accept" to "*/*", "X-Requested-With" to "XMLHttpRequest")
+        ).parsed<ResponseHash>() ?: return null
+
+        return response.embed_url.getIframe()
+    }
+
+    private fun getSourceUrl(nume: String, id: String, type: String): String? {
+        val response = app.post(
+            url = "$directUrl/wp-admin/admin-ajax.php",
+            data = mapOf(
+                "action" to "doo_player_ajax",
+                "post" to id,
+                "nume" to nume,
+                "type" to type
+            ),
+            referer = data,
+            headers = mapOf("Accept" to "*/*", "X-Requested-With" to "XMLHttpRequest")
+        ).parsed<ResponseHash>() ?: return null
+
+        return response.embed_url.getIframe()
+    }
+
+    private fun getNumeList(document: org.jsoup.nodes.Document): List<String>? {
+        return document.select("ul#playeroptionsul > li").map {
+            it.attr("data-nume")
+        }
     }
 
     private fun String.getIframe(): String {
